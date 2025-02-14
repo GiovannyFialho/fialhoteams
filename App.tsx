4c8974e31cd26d69293583cb369087a0bc327389
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-02-14 14:27:15 -0300

    carregando fonts dentro do projeto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
 
 import { Groups } from "@screens/Groups";
 import { defaultTheme } from "@theme/default";
 
 export function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
