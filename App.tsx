fc795988feeff6716730aeacc1b6d522e0360ba1
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-02-14 13:55:15 -0300

    criacao de theme e utilizacao em componentes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from "styled-components";
+
 import { Groups } from "@screens/Groups";
+import { defaultTheme } from "@theme/default";
 
 export function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
