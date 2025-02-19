0ed0d92d9e663a7ee68135e03131ab221c098473
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-02-18 23:29:45 -0300

    adicionando opcao de voltar no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
 import logoImage from "@assets/logo.png";
 
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: HeaderProps) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
       <Logo source={logoImage} />
     </Container>
   );
