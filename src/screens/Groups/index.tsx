64fa5f0350c90a61c54fa40b375a43e63d5ba7ab
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-02-19 21:58:44 -0300

    criacao de estado para armazenas grupos e exibicao com flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
-import { Container } from "./styles";
+import { Container, NoneGroup } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera do fut"]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do fut" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <NoneGroup>Nenhum grupo cadastrado</NoneGroup>
+        )}
+      />
     </Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,7 +1,16 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   padding: 24px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+`;
+
+export const NoneGroup = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.GRAY_300};
+    text-align: center;
+  `}
 `;
