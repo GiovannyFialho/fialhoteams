051c12ccf1b3e55f9a67356f67ac8cccd40357f5
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-02-20 21:26:27 -0300

    criacao do componente Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 import { Container, Content, Icon } from "./styles";
 
@@ -17,6 +18,7 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
+        <Input style={{ marginBottom: 20 }} />
         <Button title="Criar" />
       </Content>
     </Container>
