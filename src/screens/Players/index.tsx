59a72f8deb75f80dba5032a447d05cd0afb33eef
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-02-26 22:29:00 -0300

    criacao do componente PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,12 +6,15 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { PlayerCard } from "@components/PlayerCard";
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(["Giovanny"]);
+
+  function handleRemovePlayer() {}
 
   return (
     <Container>
@@ -43,6 +46,14 @@ export function Players() {
 
         <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={handleRemovePlayer} />
+        )}
+      />
     </Container>
   );
 }
