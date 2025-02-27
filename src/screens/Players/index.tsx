50b6220f9acfd6a8e6f24ab1dca8005bb5f1bb11
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-02-26 22:35:49 -0300

    ajustando visualizacao da pagina de players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
 
+import { Button } from "@components/Button";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { ListEmpty } from "@components/ListEmpty";
 import { PlayerCard } from "@components/PlayerCard";
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState(["Giovanny"]);
+  const [players, setPlayers] = useState([]);
 
   function handleRemovePlayer() {}
 
@@ -50,10 +52,20 @@ export function Players() {
       <FlatList
         data={players}
         keyExtractor={(item) => item}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={handleRemovePlayer} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
       />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
