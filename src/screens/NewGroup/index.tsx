5b4cb5f269a9809930eae4300f593a09626c568a
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-03-02 18:18:20 -0300

    implementando navegacao no projeto

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -6,6 +8,12 @@ import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group: "rocket" });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -19,7 +27,7 @@ export function NewGroup() {
         />
 
         <Input placeholder="Nome da turma" style={{ marginBottom: 20 }} />
-        <Button title="Criar" />
+        <Button title="Criar" onPress={handleNew} />
       </Content>
     </Container>
   );
