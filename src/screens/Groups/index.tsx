fa531814ad2c88217b3ee7322353173632ef8ded
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-03-04 21:33:11 -0300

    criacao de funcao para abertura de tela de config do grupo

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -26,6 +26,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate("players", { group });
+  }
+
   function handleNewGroup() {
     navigation.navigate("new");
   }
@@ -44,7 +48,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         showsVerticalScrollIndicator={false}
         ListEmptyComponent={() => (
