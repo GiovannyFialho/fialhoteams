3133007dac7ff0e0216a0c0b868639afed377102
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-03-05 20:34:40 -0300

    ajustes de usabilidade no projeto

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,6 @@
 import { useRoute } from "@react-navigation/native";
-import { useEffect, useState } from "react";
-import { Alert, FlatList } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Alert, FlatList, TextInput } from "react-native";
 
 import { AppError } from "@utils/AppError";
 
@@ -31,6 +31,8 @@ export function Players() {
   const route = useRoute();
   const { group } = route.params as RouteParams;
 
+  const newPlayerNameInputRef = useRef<TextInput>(null);
+
   async function handleAddPlayer() {
     if (newPlayerName.trim().length === 0) {
       return Alert.alert(
@@ -46,8 +48,9 @@ export function Players() {
 
     try {
       await playerAddByGroup(newPlayer, group);
-      fetchPlayersByTeam();
 
+      fetchPlayersByTeam();
+      newPlayerNameInputRef.current?.blur();
       setNewPlayerName("");
     } catch (error) {
       if (error instanceof AppError) {
@@ -87,10 +90,13 @@ export function Players() {
 
       <Form>
         <Input
+          inputRef={newPlayerNameInputRef}
           placeholder="Nome da pessoa"
           onChangeText={setNewPlayerName}
           value={newPlayerName}
           autoCorrect={false}
+          onSubmitEditing={handleAddPlayer}
+          returnKeyType="done"
         />
 
         <ButtonIcon iconName="add" onPress={handleAddPlayer} />
